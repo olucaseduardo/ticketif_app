48e5cba6b6dd8d6bfc32a4d8a1affee83cba648d
Henk-theduck <luishenrique15.lh4@gmail.com>
2022-10-29 00:47:10 -0300

    update: new login screen

--- lib/core/Services/dio_client_test.dart
+++
@@ -1,9 +0,0 @@
-import 'dio_client.dart';
-
-void main() async {
-  DioClient dioClient = DioClient('https://pokeapi.co/api/v2/pokemon');
-
-  var response = await dioClient.get('/ditto');
-
-  print(response.toString());
-}
---
+++ lib/features/resources/theme/app_colors.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+
+  static const Color white = Colors.white;
+  static const Color red = Color(0xFFF44336);
+  static const Color yellow = Color(0xFFFFA000);
+  static const Color blue = Color(0xFF2D2D87);
+
+  ///Green variants
+  static const Color green300 = Color(0xFF176426);
+  ///Green variants
+  ///
+  ///primary app color
+  static const Color green500 = Color(0xFF319945);
+  static const Color green800 = Color(0xFFB6D7BD);
+
+  ///Gray variants
+  static const Color gray200 = Color(0xFFB6D7BD);
+  static const Color gray400 = Color(0xFFB6D7BD);
+  static const Color gray700 = Color(0xFFB6D7BD);
+  static const Color gray800 = Color(0xFFB6D7BD);
+  static const Color gray900 = Color(0xFFB6D7BD);
+
+
+}
